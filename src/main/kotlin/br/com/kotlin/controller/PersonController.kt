2209725e70bb606fe10ab7cc294cf1431694987b
Jefferson William Machado <jeffersonwilliammachado@gmail.com>
2025-01-27 14:27:49 -0300

    refactor: improve request method impl

--- src/main/kotlin/br/com/kotlin/controller/PersonController.kt
+++ src/main/kotlin/br/com/kotlin/controller/PersonController.kt
@@ -4,11 +4,15 @@ import br.com.kotlin.model.Person
 import br.com.kotlin.repository.postgres.PersonPostgresRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.DeleteMapping
 
 @RestController
 @RequestMapping("/person")
@@ -16,28 +20,29 @@ class PersonController {
     @Autowired
     private lateinit var repository: PersonPostgresRepository
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun view(@PathVariable(value = "id") id: Long): Person {
         return repository.findById(id)
     }
 
-    @RequestMapping(method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun findAll(): List<Person> {
         return repository.findAll()
     }
 
-    @RequestMapping(method = [RequestMethod.POST], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun create(@RequestBody person: Person): Person {
         return repository.create(person)
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.PUT], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PutMapping(value = ["/{id}"], consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun update(@PathVariable(value = "id") id: Long, @RequestBody person: Person): Person {
         return repository.update(id, person)
     }
 
-    @RequestMapping(value = ["/{id}"], method = [RequestMethod.DELETE], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun delete(@PathVariable(value = "id") id: Long) {
+    @DeleteMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun delete(@PathVariable(value = "id") id: Long): ResponseEntity<*> {
         repository.delete(id)
+        return ResponseEntity.noContent().build<Any>()
     }
 }
